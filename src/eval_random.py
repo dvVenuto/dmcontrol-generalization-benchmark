ce197810a95c26b8d9458a6f27e95df03276f697
dvVenuto <dv.venuto@gmail.com>
2022-05-05 00:02:25 -0400

    add files

--- src/eval_random.py
+++ src/eval_random.py
@@ -73,7 +73,7 @@ def main(args):
 
     # Set working directory
     work_dir = os.path.join(args.log_dir, args.domain_name+'_'+args.task_name, args.algorithm, str(args.seed))
-    work_dir = "./"
+    work_dir = "./eval_dir"
     print('Working directory:', work_dir)
     assert os.path.exists(work_dir), 'specified working directory does not exist'
     #model_dir = utils.make_dir(os.path.join(work_dir, 'model'))
